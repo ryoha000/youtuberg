4221963ec911f893c52151f1b62ee4294ba536b8
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2021-04-28 22:46:02 +0900

    rm a part of unused code

--- src/lib/canvas.ts
+++ src/lib/canvas.ts
@@ -83,15 +83,16 @@ export const laplacianFilter = ($canvas: HTMLCanvasElement) => {
   }
   // console.log('end laplacian filter', (performance.now() - filterStart) / 1000)
 
-  const side = getSide(imageData.width)
+  // const side = getSide(imageData.width)
+  const side = 1
   const rows = Math.ceil(imageData.height / side)
   const cols = Math.ceil(imageData.width / side)
   const scores = getScores(outData, imageData.width, imageData.height, rows, cols, side)
-  const labels = new Array(rows * cols)
-  const contrours = new Array(rows * cols)
+  const labels: number[] = new Array(rows * cols)
+  const contrours: number[] = new Array(rows * cols)
   findContrours(scores, rows, cols, labels, contrours, side * side * 0.12, side * side * 0.30)
-  getMergedContrours(labels, contrours, cols)
-  fillMissingBlock(labels, rows, cols)
+  // getMergedContrours(labels, contrours, cols)
+  // fillMissingBlock(labels, rows, cols)
 
   // console.log('end merge も', (performance.now() - filterStart) / 1000)
 
@@ -121,80 +122,7 @@ export const laplacianFilter = ($canvas: HTMLCanvasElement) => {
     }
   }
   ctx.putImageData(out, 0, 0);
-
-  // setTimeout(() => {
-  //   fillMissingBlock(labels, rows, cols)
-  //   const groupIdColor: [number, number, number][] = []
-  //   for (let i = 0; i < labels.length; i++) {
-  //     if (labels[i] === 0) continue
-  //     if (!groupIdColor[labels[i]]) {
-  //       const r = getRandomInt(255)
-  //       const g = getRandomInt(255)
-  //       const b = getRandomInt(255)
-  //       groupIdColor[labels[i]] = [r, g, b]
-  //     }
-  //     const [r, g, b] = groupIdColor[labels[i]]
-  //     const row = Math.floor(i / cols)
-  //     const col = i % cols
-  //     for (let k = row * side; k < Math.min((row + 1) * side, imageData.height); k++) {
-  //       for (let l = col * side; l < Math.min((col + 1) * side, imageData.width); l++) {
-  //         outData[(k * imageData.width + l) * 4 + 0] = r
-  //         outData[(k * imageData.width + l) * 4 + 1] = g
-  //         outData[(k * imageData.width + l) * 4 + 2] = b
-  //         outData[(k * imageData.width + l) * 4 + 3] = 255
-  //       }
-  //     }
-  //   }
-  //   ctx.putImageData(out, 0, 0);
-  // }, 1000);
-
-  // fill(outData, imageData.width, imageData.height)
 }
-
-const fill = (data: Uint8ClampedArray, width: number, height: number) => {
-  const tanni = Math.ceil(width * 0.025)
-  const sikiiti = 60
-  const result = []
-  for (let i = 0; i < Math.ceil(height / tanni); i++) {
-    for (let j = 0; j < Math.ceil(width / tanni); j++) {
-      let whiteNum = 0
-      for (let k = i * tanni; k < Math.min((i + 1) * tanni, height); k++) {
-        for (let l = j * tanni; l < Math.min((j + 1) * tanni, width); l++) {
-          if (data[(k * width + l) * 4 + 0] === 255 &&
-            data[(k * width + l) * 4 + 1] === 255 &&
-            data[(k * width + l) * 4 + 2] === 255 &&
-            data[(k * width + l) * 4 + 3] === 255
-          ) {
-            whiteNum++
-          }
-        }
-      }
-      if (whiteNum > tanni * tanni * 0.50) continue
-      result.push({ i, j, whiteNum })
-    }
-  }
-  result.sort((a, b) => b.whiteNum - a.whiteNum)
-  let count = 0
-  for (const { i, j, whiteNum } of result) {
-    count++
-    // if (count > sikiiti) break
-    if (whiteNum < tanni * tanni * 0.175) break
-    for (let k = i * tanni; k < Math.min((i + 1) * tanni, height); k++) {
-      for (let l = j * tanni; l < Math.min((j + 1) * tanni, width); l++) {
-        data[(k * width + l) * 4 + 0] = 255
-        data[(k * width + l) * 4 + 1] = 255
-        data[(k * width + l) * 4 + 2] = 255
-        data[(k * width + l) * 4 + 3] = 255
-      }
-    }
-  }
-}
-
-const kernel = [
-  0, -1, 0,
-  -1, 5, -1,
-  0, -1, 0
-].map(v => v * 10)
 
 export const sharping = ($canvas: HTMLCanvasElement) => {
   const ctx = $canvas.getContext('2d')
@@ -203,11 +131,16 @@ export const sharping = ($canvas: HTMLCanvasElement) => {
   const height = $canvas.height
   const imageData = ctx.getImageData(0, 0, width, height)
   const result = new ImageData(width, height)
+  const kernel = [
+    0, -1, 0,
+    -1, 5, -1,
+    0, -1, 0
+  ].map(v => v * 10)
   for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
-      result.data[(i * width + j) * 4 + 0] = getMatrix(imageData.data, i, width, j, 0)
-      result.data[(i * width + j) * 4 + 1] = getMatrix(imageData.data, i, width, j, 1)
-      result.data[(i * width + j) * 4 + 2] = getMatrix(imageData.data, i, width, j, 2)
+      result.data[(i * width + j) * 4 + 0] = getMatrix(imageData.data, kernel, i, width, j, 0)
+      result.data[(i * width + j) * 4 + 1] = getMatrix(imageData.data, kernel, i, width, j, 1)
+      result.data[(i * width + j) * 4 + 2] = getMatrix(imageData.data, kernel, i, width, j, 2)
       result.data[(i * width + j) * 4 + 3] = imageData.data[(i * width + j) * 4 + 3]
     }
   }
@@ -220,7 +153,8 @@ const getEle = (data: Uint8ClampedArray, width: number, suffix: number, row: num
   }
   return 0
 }
-const getMatrix = (data: Uint8ClampedArray, i: number, width: number, j: number, suffix: number) => {
+
+const getMatrix = (data: Uint8ClampedArray, kernel: number[], i: number, width: number, j: number, suffix: number) => {
   return (
       kernel[0] * getEle(data, width, suffix, i - 1, j - 1)
     + kernel[1] * getEle(data, width, suffix, i - 1, j    )
@@ -233,4 +167,3 @@ const getMatrix = (data: Uint8ClampedArray, i: number, width: number, j: number,
     + kernel[8] * getEle(data, width, suffix, i + 1, j + 1)
   )
 }
-
