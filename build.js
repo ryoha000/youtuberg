ad9f9a731ed075d96269850e440180ea3acf9586
ryoha000 <the7uya@gmail.com>
2021-05-20 18:44:22 +0900

    fix build

--- build.js
+++ build.js
@@ -26,32 +26,13 @@ let wasmPlugin = {
   },
 }
 
-const copyWasm = async () => {
-  const path = require('path')
-  const fs = require('fs/promises')
-  const wasmName = 'youtuberg_wasm_bg.wasm'
-  const wasmOriginalPath = path.join(__dirname, './node_modules/youtuberg-wasm', wasmName)
-  const wasmPublicPath = path.join(__dirname, './public', wasmName)
-  try {
-    await fs.unlink(wasmPublicPath)
-  } catch {}
-  try {
-    fs.copyFile(wasmOriginalPath, wasmPublicPath)
-  } catch (e) {
-    console.error(e)
-    console.error('please run "npm install"')
-  }
-}
-
-copyWasm().then(_ => {
-  const isProduction = !!Number(process.env.PRODUCTION)
-  require('esbuild').build({
-    entryPoints: ['src/content_script.ts', 'src/patch-worker.js', 'src/worker.ts', 'src/background.ts'],
-    bundle: true,
-    outdir: 'public',
-    loader: { '.png': 'dataurl' },
-    watch: !isProduction,
-    minify: isProduction,
-    plugins: [wasmPlugin]
-  }).catch(() => process.exit(1))
-})
+const isProduction = !!Number(process.env.PRODUCTION)
+require('esbuild').build({
+  entryPoints: ['src/content_script.ts', 'src/patch-worker.js', 'src/worker.ts', 'src/background.ts'],
+  bundle: true,
+  outdir: 'public',
+  loader: { '.png': 'dataurl' },
+  watch: !isProduction,
+  minify: isProduction,
+  plugins: [wasmPlugin]
+}).catch(() => process.exit(1))
