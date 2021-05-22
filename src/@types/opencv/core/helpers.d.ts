b7f31b7cf751788fe2516957091cf45048a431f2
ryoha000 <the7uya@gmail.com>
2021-05-22 10:56:35 +0900

    rm unused code

--- src/@types/opencv/core/helpers.d.ts
+++
@@ -1,6 +0,0 @@
-import { Mat } from './Mat'
-import { int } from './_types'
-
-export function matFromArray<T>(rows: int, cols: int, type: int, array: ArrayLike<T>): Mat
-
-export function matFromImageData(imageData: ImageData): Mat
